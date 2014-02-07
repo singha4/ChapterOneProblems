c416d95ff73bd8722a4722c95a3bf069c77aa7f3
singha4 <amandeep159632478@gmail.com>
2014-02-07 10:11:03 -0500

    ChapterOneProblems1

--- ChapterOneProblems/Initials/Initials.cs
+++ ChapterOneProblems/Initials/Initials.cs
@@ -9,12 +9,12 @@ namespace Initials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("     A       SSSSSS         ");
+            Console.WriteLine("     A       SSSSS         ");
             Console.WriteLine("    A A     S           ");
-            Console.WriteLine("   A   A     SSSSSS        ");
-            Console.WriteLine("  A A A A          S  ");
-            Console.WriteLine(" A       A         S    ");
-            Console.WriteLine("A         A  SSSSSS           ");
+            Console.WriteLine("   A   A     SSSSS        ");
+            Console.WriteLine("  A A A A         S  ");
+            Console.WriteLine(" A       A        S    ");
+            Console.WriteLine("A         A  SSSSS           ");
         }
     }
 }
